d65c559a48f4998ad36738d6b9a036580d1813d9
Phojie <phojrengel@gmail.com>
2020-01-15 09:39:06 +0800

    test

--- src/pages/admin/library/borrowers.vue
+++ src/pages/admin/library/borrowers.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="q-pa-md">
-    <q-toolbar class="shadow-2 rounded-borders">
-      <q-btn flat icon="library_books" label="BOOK BORROWERS SY: 2019-2020" class="text-caption text-white bg-primary text-weight-bolder" />
+    <q-toolbar class="shadow-2 rounded-borders q-px-none">
+      <q-btn round flat icon="library_books" label="BOOK BORROWERS SY: 2019-2020" class="text-caption text-white bg-primary text-weight-bolder" />
       <q-space />
       <!--
         notice shrink property since we are placing it
         as child of QToolbar
       -->
       <q-tabs v-model="tab"  inline-label no-caps="" dense>
-        <q-tab name="tab1" class="text-blue-10" label="Education" />
-        <q-tab name="tab2" class="text-deep-purple-10" label="Criminology" />
-        <q-tab name="tab3" class="text-green-10"  label="Computer Science" />
-        <q-tab name="tab4" class="text-amber-10"  label="Business Administration" />
-        <q-separator vertical/>
-        <q-tab name="tab5" class="text-amber-10"  label="None Teaching" />
-        <q-tab name="tab6" class="text-amber-10"  label="None Teaching" />
-        <q-tab name="tab7" class="text-amber-10"  label="None Teaching" />
+        <q-tab name="tab1" class="text-blue-10" label="College Students" />
+        <q-tab name="tab3" class="text-green-10"  label="High School Students" />
+        <q-tab name="tab2" class="text-deep-purple-10" label="Personnel" />
       </q-tabs>
     </q-toolbar>
   </div>
